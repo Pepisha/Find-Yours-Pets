60068d34bee276a5649c534bdc7f7dab9ef6d727
Morgane Vidal <imala34@hotmail.fr>
2015-11-04 17:59:59 +0100

    Gestion BD : address

---
+++ Find-Yours-Pets/app/src/main/java/company/pepisha/find_yours_pets/db/adress/AddressConstants.java
@@ -0,0 +1,25 @@
+package company.pepisha.find_yours_pets.db.adress;
+
+
+public class AddressConstants {
+
+    protected static final String ADDRESS = "Address";
+
+    protected static final String ID_ADDRESS = "idAddress";
+    protected static final String STREET = "street";
+    protected static final String ZIPCODE = "zipcode";
+    protected static final String CITY = "city";
+    protected static final String LATITUDE = "latitude";
+    protected static final String LONGITUDE = "longitude";
+
+    protected static final String ADDRESS_CREATE =  "create table " + ADDRESS
+            + "(" + ID_ADDRESS + " integer primary key autoincrement, " +
+            STREET + " integer not null, " +
+            ZIPCODE + " text not null, " +
+            CITY + " text not null, " +
+            LATITUDE + " text not null, " +
+            LONGITUDE + " text not null " +
+            ");";
+
+    protected static final String ADDRESS_DROP = "DROP TABLE IF EXISTS " + ADDRESS;
+}
