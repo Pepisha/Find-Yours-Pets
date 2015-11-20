715fca4feb8d516f6fb0f06f8a8978936e805e58
Manouel <manuel.cha34@gmail.com>
2015-11-20 17:45:23 +0100

    Connexion au server et envoi des requêtes connexion et inscription

--- Find-Yours-Pets/app/src/main/java/company/pepisha/find_yours_pets/RegistrationActivity.java
+++ Find-Yours-Pets/app/src/main/java/company/pepisha/find_yours_pets/RegistrationActivity.java
@@ -1,6 +1,7 @@
 package company.pepisha.find_yours_pets;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,8 +11,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import company.pepisha.find_yours_pets.db.user.UserOperation;
-import company.pepisha.find_yours_pets.tools.RegistrationCheck;
+import java.util.HashMap;
+
+import company.pepisha.find_yours_pets.connection.ServerDbOperation;
 
 public class RegistrationActivity extends Activity {
 
@@ -25,39 +27,46 @@ public class RegistrationActivity extends Activity {
         registrationButton.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                UserOperation userDBOperation = new UserOperation(v.getContext());
-                try {
-                    userDBOperation.open();
 
-                    EditText nickname = (EditText) findViewById(R.id.nickname);
-                    EditText password = (EditText) findViewById(R.id.password);
-                    EditText passwordConfirmation = (EditText) findViewById(R.id.passwordConfirmation);
-                    EditText mail = (EditText) findViewById(R.id.mail);
-                    EditText phone = (EditText) findViewById(R.id.phone);
-                    EditText firstname = (EditText) findViewById(R.id.firstname);
-                    EditText lastname = (EditText) findViewById(R.id.lastname);
+                EditText nickname = (EditText) findViewById(R.id.nickname);
+                EditText password = (EditText) findViewById(R.id.password);
+                EditText passwordConfirmation = (EditText) findViewById(R.id.passwordConfirmation);
+                EditText mail = (EditText) findViewById(R.id.mail);
+                EditText phone = (EditText) findViewById(R.id.phone);
+                EditText firstname = (EditText) findViewById(R.id.firstname);
+                EditText lastname = (EditText) findViewById(R.id.lastname);
 
-                    if (RegistrationCheck.passwordsMatch(password.getText().toString(), passwordConfirmation.getText().toString())) {
+                HashMap<String, String> request = new HashMap<String, String>();
+                request.put("page", "register");
+                request.put("nickname", nickname.getText().toString());
+                request.put("password1", password.getText().toString());
+                request.put("password2", passwordConfirmation.getText().toString());
+                request.put("mail", mail.getText().toString());
+                request.put("phone", phone.getText().toString());
+                request.put("firstname", firstname.getText().toString());
+                request.put("lastname", lastname.getText().toString());
 
-                        userDBOperation.addUser(nickname.getText().toString(),
-                                password.getText().toString(),
-                                mail.getText().toString(),
-                                phone.getText().toString(),
-                                firstname.getText().toString(),
-                                lastname.getText().toString());
-                    }
-
-                    Toast.makeText(getApplicationContext(), "Inscription de " + nickname.getText().toString(), Toast.LENGTH_LONG).show();
-
-                    Intent homeScreen = new Intent(getApplicationContext(), HomeActivity.class);
-                    startActivity(homeScreen);
-                } catch(android.database.SQLException e) {
-                    Toast.makeText(getApplicationContext(), "DB error :" + e.toString(), Toast.LENGTH_LONG).show();
-                }
-
-                userDBOperation.close();
+                new RegistrationDbOperation(getApplicationContext()).execute(request);
             }
         });
+    }
+
+    private class RegistrationDbOperation extends ServerDbOperation {
+        public RegistrationDbOperation(Context c) {
+            super(c);
+        }
+
+        @Override
+        protected void onPostExecute(HashMap<String, String> result) {
+            if (result.get("success").equals("true")) {
+                Toast.makeText(getApplicationContext(), "Inscription réussie", Toast.LENGTH_LONG).show();
+
+                Intent homeScreen = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(homeScreen);
+            } else {
+                Toast.makeText(getApplicationContext(), result.get("error"), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
