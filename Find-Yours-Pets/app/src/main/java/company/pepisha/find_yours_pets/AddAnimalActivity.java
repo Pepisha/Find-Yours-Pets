183d1d30b66feccb9fdfbdac51dba4ac0a58ba75
Morgane Vidal <imala34@hotmail.fr>
2015-12-10 09:45:10 +0100

    Facebook share animal on create

--- Find-Yours-Pets/app/src/main/java/company/pepisha/find_yours_pets/AddAnimalActivity.java
+++ Find-Yours-Pets/app/src/main/java/company/pepisha/find_yours_pets/AddAnimalActivity.java
@@ -2,6 +2,7 @@ package company.pepisha.find_yours_pets;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,18 +11,37 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
+
 import java.util.HashMap;
 import java.util.List;
 
 import company.pepisha.find_yours_pets.connection.ServerDbOperation;
 import company.pepisha.find_yours_pets.db.animal.AnimalConstants;
-import company.pepisha.find_yours_pets.db.animal.AnimalStateConstants;
 import company.pepisha.find_yours_pets.db.animal.animalType.AnimalType;
 import company.pepisha.find_yours_pets.db.animal.animalType.AnimalTypeOperation;
 
 public class AddAnimalActivity extends BaseActivity {
 
     private AnimalTypeOperation animalTypeDbOperation;
+    ShareDialog shareDialog;
+
+    private class AddAnimalDbOperation extends ServerDbOperation {
+        public AddAnimalDbOperation(Context c) {
+            super(c, "addAnimalInShelter");
+        }
+
+        @Override
+        protected void onPostExecute(HashMap<String, Object> result) {
+            if (successResponse(result)) {
+                Toast.makeText(getApplicationContext(), R.string.animalAdded, Toast.LENGTH_SHORT).show();
+
+                Intent homeScreen = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(homeScreen);
+            }
+        }
+    }
 
     private void animalAdding() {
         EditText animalName = (EditText) findViewById(R.id.animalName);
@@ -52,6 +72,43 @@ public class AddAnimalActivity extends BaseActivity {
         new AddAnimalDbOperation(getApplicationContext()).execute(request);
     }
 
+    private void shareAnimalOnFacebook() {
+        EditText animalName = (EditText) findViewById(R.id.animalName);
+        RadioGroup radioGroupAnimalTypes = (RadioGroup) findViewById(R.id.radioGroupAnimalTypes);
+        RadioGroup radioGroupAnimalGender = (RadioGroup) findViewById(R.id.radioGroupAnimalSexe);
+        EditText breedAnimal = (EditText) findViewById(R.id.breedAnimal);
+        EditText ageAnimal = (EditText) findViewById(R.id.ageAnimal);
+        EditText catsFriend = (EditText) findViewById(R.id.catsFriend);
+        EditText dogsAgreements = (EditText) findViewById(R.id.dogsAgreements);
+        EditText childrenAgreements = (EditText) findViewById(R.id.childrenAgreements);
+        EditText description = (EditText) findViewById(R.id.description);
+
+        String postTitle = animalName.getText().toString();
+        String postContent = animalName.getText().toString()+"\n"
+                + breedAnimal.getText().toString()+"\n"
+                + ageAnimal.getText().toString()+"\n"
+                + catsFriend.getText().toString()+"\n"
+                + dogsAgreements.getText().toString()+"\n"
+                + childrenAgreements.getText().toString()+"\n"
+                + description.getText().toString();
+
+        ShareLinkContent contentPost = new ShareLinkContent.Builder()
+                .setContentTitle(postTitle)
+                .setContentDescription(postContent)
+                .build();
+        ShareLinkContent linkContent = new ShareLinkContent.Builder()
+                .setContentTitle(animalName.getText().toString())
+                .setContentDescription(
+                        postContent)
+                .setContentUrl(Uri.parse("http://www.find-yours-pets.esy.es/"))
+                .build();
+
+        shareDialog = new ShareDialog(this);
+        shareDialog.show(linkContent);
+
+        //TODO ajouter image quand l'upload d'images marchera
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,6 +122,7 @@ public class AddAnimalActivity extends BaseActivity {
 
             public void onClick(View v) {
                 animalAdding();
+                shareAnimalOnFacebook();
             }
         });
     }
@@ -86,19 +144,5 @@ public class AddAnimalActivity extends BaseActivity {
         }
     }
 
-    private class AddAnimalDbOperation extends ServerDbOperation {
-        public AddAnimalDbOperation(Context c) {
-            super(c, "addAnimalInShelter");
-        }
 
-        @Override
-        protected void onPostExecute(HashMap<String, Object> result) {
-            if (successResponse(result)) {
-                Toast.makeText(getApplicationContext(), R.string.animalAdded, Toast.LENGTH_SHORT).show();
-
-                Intent homeScreen = new Intent(getApplicationContext(), HomeActivity.class);
-                startActivity(homeScreen);
-            }
-        }
-    }
 }
