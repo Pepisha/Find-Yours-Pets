b29fd90105cb2f35a791480d5ed2be9ec0848735
Morgane Vidal <imala34@hotmail.fr>
2015-12-11 10:31:04 +0100

    mise en place du menu andmin et choix du menu en fonction de si l'utilisateur est connecté ou non.

--- Find-Yours-Pets/app/src/main/java/company/pepisha/find_yours_pets/MainActivity.java
+++ Find-Yours-Pets/app/src/main/java/company/pepisha/find_yours_pets/MainActivity.java
@@ -80,10 +80,4 @@ public class MainActivity extends BaseActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_disconnected, menu);
-        return true;
-    }
 }
